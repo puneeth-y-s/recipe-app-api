4e8962e9d0156a1f3c3e9d12f3ec0270dac7ead6
puneeth-y-s <yspuneeth1994@gmail.com>
2023-05-06 13:21:34 +0530

    postgresql related config changes

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator functions
+
+"""
+    
+def add(x, y):
+    "Add x and y and return result"
+    return x + y
+
+def subtract(x, y):
+    "Subtract x and y and return result"
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+sample tests
+ 
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Test the calc module. """
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(10, 15)
+        self.assertEqual(res, 25)
+        
+    def test_subtract_numbers(self):
+        """Test subtracting numbers"""
+        res = calc.subtract(10, 7)
+        self.assertEqual(res, 3)
+ 
+    
+
